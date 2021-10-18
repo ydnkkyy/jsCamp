c09d2cbcb47f7731eaee8db3391795c8a10df882
AKKAYA <ydn@AKKAYA-MacBook-Pro.local>
2021-10-19 00:03:21 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("Merhaba Kodlama.io")
+
+// JS type safe değildir
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+const euroDun = 11.20
+
+console.log(euroDun)
+
+//array
+let konutKredileri = ["Özel Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Taşıt Kredileri"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"<li>")
+    
+}
+
+
+console.log(konutKredileri)
